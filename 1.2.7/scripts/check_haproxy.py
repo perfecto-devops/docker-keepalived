965cc20d7aa515add50511937033a77296984772
Perfecto DevOps <service-ops@perfectomobile.com>
2016-08-04 16:57:50 -0400

    Update check_haproxy.py

--- 1.2.7/scripts/check_haproxy.py
+++ 1.2.7/scripts/check_haproxy.py
@@ -1,4 +1,5 @@
 #!/bin/bash
-if [[ `curl -sL -w "%{http_code}\\n" -o /dev/null --connect-timeout 1 $1` -ne "200" ]]; then
-        exit 1
-fi
+#if [[ `curl -sL -w "%{http_code}\\n" -o /dev/null --connect-timeout 1 $1` -ne "200" ]]; then
+#        exit 1
+#fi
+exit 0
